bd9c53fabc646842471affd05297776491bb8507
ESS Mac Pro <ESSMacPro@esss-macbook-pro.local>
2017-03-28 01:56:12 -0400

    Initial Commit

---
+++ TextView-Tips&Tricks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextView-Tips&Tricks
+//
+//  Created by ESS Mac Pro on 3/28/17.
+//  Copyright © 2017 NGA Group Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
